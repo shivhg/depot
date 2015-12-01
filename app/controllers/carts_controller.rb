e209d65f001a8ed369cd8ed160b12880d616957d
shivhg <shivhg@gmail.com>
2015-12-02 01:16:14 +0530

    smart cart functionality added

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,7 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
+
 
   # GET /carts
   # GET /carts.json
@@ -54,21 +56,29 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to root_url,
+                                notice: 'Your cart is currently empty' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_cart
-      @cart = Cart.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_cart
+    @cart = Cart.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def cart_params
-      params[:cart]
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def cart_params
+    params[:cart]
+  end
+
+  def invalid_cart
+    logger.error "attempt to access invalid id #{params[:id]}"
+    flash[:notice] = 'Invalid cart'
+    redirect_to root_url
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151126195222) do
+ActiveRecord::Schema.define(version: 20151201175014) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
